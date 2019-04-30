de3c38eecb2e524a9a9421e0a6978448662cd016
DouglasHT <doug.wow@hotmail.com>
2019-04-30 15:58:01 -0300

    Create Assembly-CSharp.csproj

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] int breackableBlocks; // Serialized for debuggin purposes
+
+
+
+    // cached paameters
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
+
+    public void CountBlocks()
+    {
+        breackableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breackableBlocks--;
+
+        if (breackableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
